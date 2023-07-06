58635703a0931b33a9bcf09d9ce07c0f5bf09a59
andrader <38709237+andrader@users.noreply.github.com>
2023-07-05 22:29:27 -0300

    translate code to english

--- main.py
+++ main.py
@@ -1,77 +1,56 @@
 # Author: Rubens Andrade Filho
 # Date: 2023-07-05
 
-def resolver(pedidos: list[int], rolos: list[int]):
-    # print(f"resolver {pedidos=}, {rolos=}")
-    if len(pedidos)==0:
+
+def resolver(orders: list[int], rolls: list[int]):
+    if len(orders) == 0:
         return []
-    
-    pedido = pedidos[0]
-    for i, rolo in enumerate(rolos):
-        # se esse rolo atende o pedido atual
-        if rolo - pedido >= 0:
-            # tenta resolver os outros pedidos com o que sobra
-            # print(f"tentando rolo {i}: {rolo}")
-            rolos_restantes = rolos[:]
-            rolos_restantes[i] = rolo - pedido
-            res = resolver(pedidos[1:], rolos_restantes)
+
+    order = orders[0]
+    for i, roll in enumerate(rolls):
+        if roll - order >= 0:
+            remaining_rolls = rolls[:]
+            remaining_rolls[i] = roll - order
+            res = resolver(orders[1:], remaining_rolls)
             if res is not False:
-                # se deu certo resolver o restante,
-                # retorna que esse pedido foi resolvido pelo rolo i 
-                # e o restante das posicoes dos rolos que resolvem cada pedido em ordem
                 return i, *res
-            
-        # se deu ruim, tenta o proximo rolo
+
     else:
-        # todos os rolos deram ruim
-        # print(f"voltando atrás de resolver {pedidos=}, {rolos=}")
         return False
 
 
-def print_resolver(pedidos: list[int], rolos: list[int]):
-    
-    
-    print(
-        f"Há {len(pedidos)} pedidos para serem atendidos:\n\t"
-        + "\n\t".join([f"pedido {i}: {p}" for i, p in enumerate(pedidos)])
-    )
-    
+def print_resolver(orders: list[int], rolls: list[int]):
+    print(f"There are {len(orders)} orders to be fulfilled:\n\t"
+          + "\n\t".join([f"Order {i}: {o}" for i, o in enumerate(orders)]))
 
-    res = resolver(pedidos, rolos)
+    res = resolver(orders, rolls)
 
     if res is not False:
+        print(f"Optimal allocation to fulfill {len(orders)} orders:\n\t"
+              + "\n\t".join([f"Order {i}: cut {o} m from roll {r}" for i, (o, r) in enumerate(zip(orders, res))]))
 
-        print(
-            f"Maneira para atender a encomenda de {len(pedidos)} pedidos:\n\t"
-            + "\n\t".join([f"pedido {i}: corte {p} m do rolo {r}" for i, (p, r) in enumerate(zip(pedidos, res))])
-        )
-
-        sobras = rolos[:]
-        for p, r in enumerate(res):
-            sobras[r] -= pedidos[p]
-        print(
-            f"Sobras no estoque:\n\t"
-            + "\n\t".join([f"rolo {i}: {sobra} m" for i, sobra in enumerate(sobras)])
-        )
+        leftovers = rolls[:]
+        for o, r in enumerate(res):
+            leftovers[r] -= orders[o]
+        print(f"Leftover rolls in stock:\n\t"
+              + "\n\t".join([f"Roll {i}: {leftover} m" for i, leftover in enumerate(leftovers)]))
     else:
-        print("Nao foi possivel atender o pedido")
-
-    
+        print("Unable to fulfill the order")
 
 
 def main():
+    rolls = input("Rolls in stock\t>>> ")
+    rolls = [int(r) for r in rolls.split()]
 
-    rolos = input(f"Rolos no estoque\t>>> ")
-    rolos = [int(r) for r in rolos.split()]
+    orders = input("Order of rolls\t>>> ")
+    orders = [int(r) for r in orders.split()]
 
-    pedidos = input(f"Encomendas de rolos\t>>> ")
-    pedidos = [int(r) for r in pedidos.split()]
+    print_resolver(orders, rolls)
 
-    print_resolver(pedidos, rolos)
 
-print("#"*80)
-print_resolver([50,15,30], [100])
-print("#"*80)
-print_resolver([70,90,70], [100, 150])
-print("#"*80)
+print("#" * 80)
+print_resolver([50, 15, 30], [100])
+print("#" * 80)
+print_resolver([70, 90, 70], [100, 150])
+print("#" * 80)
 main()
